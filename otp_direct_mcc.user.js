ee45e6717e217a173b75219805c700992498a0c4
Alexandr Zhuravlev <lev-money@mail.ru>
2019-10-24 21:54:15 +0300

    Increase version after merge PR for sovcombank

--- otp_direct_mcc.user.js
+++ otp_direct_mcc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         OTP MCC Codes
 // @namespace    http://tampermonkey.net/
-// @version      0.36
+// @version      0.37
 // @description  Show MCC in OTP Direct
 // @author       alezhu
 // @match        https://direkt.otpbank.ru/homebank/do/bankkartya/szamlatortenet*
